319477f2f5e2d021a4efe271a5915a7ce1624b35
crystall.101600 <crystall.101600@gmail.com>
2025-03-01 12:11:35 +0000

    UI for the chat screen.

---
+++ lib/chat_page.dart
@@ -0,0 +1,62 @@
+import 'package:flash/constants.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                //Implement logout functionality
+              },
+            ),
+        ],
+        title: const Text('CHAT'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: messageContainerDecoration,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        //Do something with the user input.
+                      },
+                      decoration: messageTextFieldDecoration,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      //Implement send functionality.
+                    },
+                    child: const Text(
+                      'Send',
+                      style: sendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
