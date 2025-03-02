ae966dd185f2ade0f399ecaf1772de0f08879521
crystall.101600 <crystall.101600@gmail.com>
2025-03-02 19:47:04 +0000

    Explored different types of animations and added a color tween.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,7 +3,7 @@ import 'package:flash/registration_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
- const HomePage({super.key});
+  const HomePage({super.key});
 
   static const String id = 'Home Page';
 
@@ -11,11 +11,41 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+      upperBound: 100,
+    );
+    animation = ColorTween(
+      begin: Colors.deepPurple,
+      end: Colors.deepPurpleAccent,
+    ).animate(controller);
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+      print(controller.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value as Color?,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: Column(
@@ -33,16 +63,11 @@ class _HomePageState extends State<HomePage> {
                 ),
                 const Text(
                   'F L A S H   C H A T',
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.w900,
-                  ),
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.w900),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 50,
-            ),
+            const SizedBox(height: 50),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 15),
               child: Material(
@@ -51,19 +76,17 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.circular(30),
                 child: MaterialButton(
                   onPressed: () {
-                   //LOGIN SCREEN
-                   Navigator.push(
-                       context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
-                        ),
-                     );
+                    //LOGIN SCREEN
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
                   },
                   minWidth: 200,
                   height: 42,
-                  child: const Text(
-                    'LOG IN',
-                  ),
+                  child: const Text('LOG IN'),
                 ),
               ),
             ),
@@ -77,17 +100,15 @@ class _HomePageState extends State<HomePage> {
                   onPressed: () {
                     //REGISTRATION PAGE
                     Navigator.push(
-                       context,
-                       MaterialPageRoute(
+                      context,
+                      MaterialPageRoute(
                         builder: (context) => const RegistrationPage(),
-                       ),
+                      ),
                     );
                   },
                   minWidth: 200,
                   height: 42,
-                  child: const Text(
-                    'REGISTER',
-                  ),
+                  child: const Text('REGISTER'),
                 ),
               ),
             ),
