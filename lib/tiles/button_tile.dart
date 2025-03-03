94c8089dc18685642bb42605a1d349b170a727d9
crystall.101600 <crystall.101600@gmail.com>
2025-03-03 18:19:05 +0000

    Common tile for the buttons.

---
+++ lib/tiles/button_tile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ButtonTile extends StatelessWidget {
+  const ButtonTile({
+    required this.color,
+    required this.text,
+    required this.onPressed,
+    super.key,
+  });
+
+  final Color color;
+  final String text;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30),
+        elevation: 5,
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200,
+          height: 42,
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
