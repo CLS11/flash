036834d1d6a370e49da0ebc1af790d7c8174bee1
crystall.101600 <crystall.101600@gmail.com>
2025-03-03 17:02:53 +0000

    Added the appropriate functionalities to the tiles.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash/pages/login_page.dart';
+import 'package:flash/pages/registration_page.dart';
+import 'package:flash/tiles/login_tile.dart';
+import 'package:flash/tiles/registration_tile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  static const String id = 'Home Page';
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+      upperBound: 100,
+    );
+    animation = ColorTween(
+      begin: Colors.deepPurple,
+      end: Colors.deepPurpleAccent,
+    ).animate(controller);
+
+    controller
+    ..forward()
+    ..addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value as Color?,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: const ['F L A S H   C H A T'],
+                  textStyle: const TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            LoginTile(
+              color: Colors.lightBlueAccent,
+              text: 'LOG IN',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage(),
+                  ),
+                );
+              },
+            ),
+            RegistrationTile(
+              color: Colors.blueAccent,
+              text: 'REGISTER',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegistrationPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
