e0ffa96825ab7a7255d7386c8da5fad65b79f8b3
crystall.101600 <crystall.101600@gmail.com>
2025-03-03 17:01:15 +0000

    Organised and moved to a different folder.

---
+++ lib/pages/registration_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({super.key});
+
+  static const String id = 'Registration Page';
+
+  @override
+  State<RegistrationPage> createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200, 
+                child: Image.asset('images/logo.png'),
+                ),
+            ),
+            const SizedBox(height: 48),
+            TextField(
+              onChanged: (value) {
+                //USER INPUT
+              },
+              decoration: const InputDecoration(
+                hintText: 'Enter your email',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: const InputDecoration(
+                hintText: 'Enter your password',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Material(
+                color: Colors.blueAccent,
+                borderRadius: const BorderRadius.all(Radius.circular(30)),
+                elevation: 5,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Implement registration functionality.
+                  },
+                  minWidth: 200,
+                  height: 42,
+                  child: const Text(
+                    'REGISTER',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
