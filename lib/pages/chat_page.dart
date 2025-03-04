64969e4fa875228b07aaa25eaa8fa24f30a1c531
crystall.101600 <crystall.101600@gmail.com>
2025-03-04 06:25:55 +0000

    Added firebase authentication and methodn to log out the user.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,24 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final _auth = FirebaseAuth.instance;
+  User? loggedInUser;
+  @override
+  void initState(){
+    super.initState();
+    getCurrentUser();
+  }
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser!;
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +40,8 @@ class _ChatPageState extends State<ChatPage> {
             icon: const Icon(Icons.close),
             onPressed: () {
               //Implement logout functionality
+              _auth.signOut();
+              Navigator.pop(context);
             },
           ),
         ],
