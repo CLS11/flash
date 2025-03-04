2cdcfcfcd05dcb30a9eee49fb7696523af8e3fd8
crystall.101600 <crystall.101600@gmail.com>
2025-03-04 06:27:41 +0000

    Added firebase authentication to login the user an directing to the chat page.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash/constants.dart';
+import 'package:flash/pages/chat_page.dart';
 import 'package:flash/tiles/button_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key,});
+  const LoginPage({super.key});
 
   static const String id = 'Login Page';
 
@@ -12,6 +14,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _auth = FirebaseAuth.instance;
+  late final String email;
+  late final String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,14 +30,18 @@ class _LoginPageState extends State<LoginPage> {
             Hero(
               tag: 'logo',
               child: Container(
-                height: 200, 
+                height: 200,
                 child: Image.asset('images/logo.png'),
               ),
             ),
             const SizedBox(height: 48),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
                 //Do something with the user input.
+                email:
+                value;
               },
               decoration: textFieldDecoration.copyWith(
                 hintText: 'Enter your email',
@@ -40,8 +49,12 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 8),
             TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
               onChanged: (value) {
                 //Do something with the user input.
+                password:
+                value;
               },
               decoration: textFieldDecoration.copyWith(
                 hintText: 'Enter your password',
@@ -49,9 +62,26 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 24),
             ButtonTile(
-              color: Colors.lightBlueAccent, 
-              text: 'LOG IN', 
-              onPressed: (){},
+              color: Colors.lightBlueAccent,
+              text: 'LOG IN',
+              onPressed: () async {
+                try {
+                  final user = await _auth.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+                  if (user != null) {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatPage(),
+                      ),
+                    );
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
             ),
           ],
         ),
