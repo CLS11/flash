7aac687c0e3c714c0f680cd9845a02196d6f04b4
crystall.101600 <crystall.101600@gmail.com>
2025-03-08 07:29:13 +0000

    Added show spinner until user is registered.

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash/constants.dart';
+import 'package:flash/pages/chat_page.dart';
 import 'package:flash/tiles/button_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flash/pages/chat_page.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
@@ -15,73 +16,79 @@ class RegistrationPage extends StatefulWidget {
 
 class _RegistrationPageState extends State<RegistrationPage> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   late final String email;
   late final String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(height: 48),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //USER INPUT
-                email =
-                value;
-              },
-              decoration: textFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              const SizedBox(height: 48),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //USER INPUT
+                  email = value;
+                },
+                decoration: textFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Do something with the user input.
-                password =
-                value;
-              },
-              decoration: textFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+              const SizedBox(height: 8),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                decoration: textFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            ButtonTile(
-              color: Colors.blueAccent,
-              text: 'REGISTER',
-              onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ChatPage(),
-                    ),
-                  );
+              const SizedBox(height: 24),
+              ButtonTile(
+                color: Colors.blueAccent,
+                text: 'REGISTER',
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ChatPage()),
+                    );
+                    setState(() {
+                      showSpinner = false;
+                    });
                   } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
